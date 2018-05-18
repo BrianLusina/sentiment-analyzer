3dc16574359ea58fe24cf2db38deb0628d4b2d7d
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-05-18 09:12:36 +0300

    create http client for webapp

---
+++ webapp/src/main/kotlin/Constants.kt
@@ -0,0 +1,1 @@
+const val LOGIC_SERVICE_URL = "http://0.0.0.0:5050/analyse/sentiment"
--- webapp/src/main/kotlin/WebApp.kt
+++ webapp/src/main/kotlin/WebApp.kt
@@ -1,4 +1,7 @@
 import io.ktor.application.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.request.post
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
 import io.ktor.http.*
@@ -7,31 +10,41 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.util.url
 import java.text.DateFormat
 
 
 fun main(args: Array<String>) {
+    val client = HttpClient(Apache)
+
     val server = embeddedServer(Netty, port = 8080) {
+
         install(ContentNegotiation){
             gson {
                 setDateFormat(DateFormat.LONG)
                 setPrettyPrinting()
             }
         }
+
         routing {
 
             get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
+                call.respondText("Sentiment Analyser!", ContentType.Text.Plain)
             }
 
             post("/sentiment"){
                 val sentence = call.receive<SentenceEntity>()
-//
-//                post("http://0.0.0.0:5050/analyse/sentiment") {
-//
-//                }
+
+                // post to logic service url
+                val sentiment = client.post<SentimentEntity>(LOGIC_SERVICE_URL){
+                    body = sentence
+                }
+
+                // respond to client (front end)
+                call.respond(SentimentEntity(sentiment.sentence, sentiment.polarity))
             }
         }
+
     }
     server.start(wait = true)
 }
